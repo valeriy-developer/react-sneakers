3ef6e22defc214ed98530246a04eb647edfce7a6
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-01-19 11:49:09 +0200

    feature: remove layout from components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import Layout from "./components/Layout";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import Orders from "./pages/Orders";
+import Header from "./components/Header";
+import Popup from "./components/Popup";
 
 function App() {
   return (
-    <>
+    <div className='global-container'>
+      <Popup/>
+      <Header/>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="*" element={<Navigate to="/home" replace />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
